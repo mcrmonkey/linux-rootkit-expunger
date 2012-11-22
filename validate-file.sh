a73daac61a350367705033fb2da92eff1c63466d
Ant Kenworthy <antkenworthy83@googlemail.com>
2012-11-22 00:42:22 +0000

    Update validate-file.sh
    basically re-wrote it

--- validate-file.sh
+++ validate-file.sh
@@ -1,28 +1,97 @@
 #!/bin/sh
 
-# You need to be able to send email
-# I used configuration information from http://askubuntu.com/questions/12917/how-to-send-mail-from-the-command-line
+## You need to be able to send email
 
-DIR_TO_SAVE_INTO=$HOME/.rootkit-expunger-saved-info
-MAIL_ADDR=your-email-address@yahoo.com
+## This is a quick and dirty check of rc.local to see if it has been modified after it has been initially run.
+## knowledge of the before md5sum would be good
+## md5sum can be changed for sha1sum if need be
 
-if test -f /etc/rc.local ; then
-	if test -d $DIR_TO_SAVE_INTO ; then
-		:
+
+
+
+
+
+## Edit the line below to say something other then "YES" to run the script.
+## Ensure that you have set the variables above to match your system
+
+SHOWHELP="YES"
+
+
+
+
+
+
+FILETOCHECK=/etc/rc.local
+
+DIR_TO_SAVE_INTO=$HOME/.bananaapple
+
+MAIL_ADDR=your-email@address
+
+
+
+MD5SUM=/usr/bin/md5sum
+MKDIR=/bin/mkdir
+MAIL=/bin/mail
+
+
+
+
+
+
+
+
+
+if [ $SHOWHELP == "YES" ]; then
+
+
+printf "Edit the variables in this script first !\n\n\n File to check: $FILETOCHECK \n Place of saving: $DIR_TO_SAVE_INTO \n Email address: $MAIL_ADDR\n\n md5sum location: $MD5SUM \n mkdir location: $MKDIR \n mail location: $MAIL \n\n"
+
+exit 0
+fi
+
+
+
+## Initial Run ?
+
+if [ -s $DIR_TO_SAVE_INTO/md5list ]
+	then
+		## MD5 file is non zero so just do a comapre
+		$MD5SUM --status -c $DIR_TO_SAVE_INTO/md5list
+
+		## how did md5sum die ?
+
+		if [ $? != 0 ]; then
+		
+			## something is wrong... send email or just echo something.
+			## if this is runing as a cronjob then output will be emailed via cron anyway...
+		
+			echo "Files listed in $DIR_TO_SAVE_INTO/md5list didn't seem to match their md5sum. You may wish to investigate!"
+		
+			#echo "Files listed in $DIR_TO_SAVE_INTO/md5list didn't seem to match their md5sum. You may wish to investigate!" | $MAIL -s "File signatures" $MAIL_ADDR
+
+
+		else
+
+			# All was fine with the md5's, bye.
+			exit 0
+
+		 fi
+
+
+
+
+
 	else
-		mkdir -p $DIR_TO_SAVE_INTO
-		cp /etc/rc.local $DIR_TO_SAVE_INTO
-		sha1sum /etc/rc.local >$DIR_TO_SAVE_INTO/initial.state.sum
-	fi
 
-	if diff $DIR_TO_SAVE_INTO/rc.local /etc/rc.local ; then
-		ssmtp $MAIL_ADDR <<XXxxXX
-To: $MAIL_ADDR
-From: $MAIL_ADDR
-Subject: Rootkit found on $(hostname)
-validate-file.sh found a potential rootkit on $(hostname)
-XXxxXX
-	fi
+		## Looks like the md5list is zero bytes. Initial run
+		echo " It looks like this is the first time you have run this. trying to create md5list file..."		
+		$MKDIR -p $DIR_TO_SAVE_INTO	
+	
+
+		$MD5SUM $FILETOCHECK > $DIR_TO_SAVE_INTO/md5list
+
+
+
 fi
 
 
